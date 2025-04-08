98e10ffb7638218fbb46adbdfb44a23555078da8
Moonflower-labs <134706556+Moonflower-labs@users.noreply.github.com>
2025-04-08 20:44:26 +0100

    remove css gradient from header, sync css changes

--- components/header.tsx
+++ components/header.tsx
@@ -1,13 +1,14 @@
-import auraLogo from "../src/assets/aura-logo.png"
+import auraLogo from "../src/assets/aura-logo.png";
+
 interface HeaderProps {
-    style?: string;
+    className?: string;
 }
 
-export default function Header({ style }: HeaderProps) {
+export default function Header({ className }: HeaderProps) {
     return (
-        <header className={`flex items-center justify-center p-2 rounded-md text-white/80 ${style} transition-all duration-300`}>
-            <img src={auraLogo} alt="logo" className='w-20 animate-pulse' />
-            <div className='text-4xl font-bold'>AuraVibes</div>
+        <header className={`flex items-center justify-center gap-4 p-2 px-4 rounded-md text-white/80 ${className} transition-all duration-300 ease-in-out`}>
+            <img src={auraLogo} alt="logo" className="w-20 animate-pulse" />
+            <div className="text-4xl font-bold">AuraVibes</div>
         </header>
-    )
+    );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/auravibes/",
+  base: "/auravibes/", // Change this to your GitHub Pages repository name
   plugins: [tailwindcss(), react()],
 });
